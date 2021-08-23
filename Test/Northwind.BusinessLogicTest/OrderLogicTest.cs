70ddfc7310c4507932079cf35998124a51d1678e
rgramos <riki_german@yahoo.com.ar>
2021-08-23 17:08:10 -0300

    "second commit"

--- Test/Northwind.BusinessLogicTest/OrderLogicTest.cs
+++ Test/Northwind.BusinessLogicTest/OrderLogicTest.cs
@@ -24,6 +24,7 @@ namespace Northwind.BusinessLogicTest
       var result = _orderLogic.GetOrderNumber(1);
       result.Should().NotBeNull();
       result.Should().NotBeEmpty();
+    
     }
   }
 }
